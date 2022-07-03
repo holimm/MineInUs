f07a6952e23dbc1cf39d81b76ead9129e3e83205
Nguyễn Lim Thái Hồ <95845053+holimm@users.noreply.github.com>
2022-07-03 15:02:23 +0700

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,5 +1,4 @@
 <?php
-//"51.79.156.39","miuweb","3DyF7%qhXtE-tW>*","mineinus" 05FC6A286D0735D46056BAD5787F9BD6
 	$apikey = ''; //API key, lấy từ website thesieutoc.net thay vào trong cặp dấu ''
 	
 	// database Mysql config
